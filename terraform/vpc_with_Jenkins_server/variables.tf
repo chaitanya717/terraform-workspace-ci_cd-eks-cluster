9a2b6a67aa14c02dc76844f1e3fc5ff80ec977e5
chaitanya717 <chaudharichaitanya998@gmail.com>
2024-09-08 22:09:10 +0530

    done

--- terraform/vpc_with_Jenkins_server/variables.tf
+++ terraform/vpc_with_Jenkins_server/variables.tf
@@ -65,14 +65,11 @@ variable "user_data__kubectl" {}
 # security group variables
 variable "sg_name" {}
 variable "sg_name_softwares" {}
-variable "sg_name_eks" {}
+
 variable "vpc_sg_allowports" {
     type = list(number)
 }
 variable "vpc_sg_allowports_softwares" {
-    type = list(number)
-}
-variable "vpc_sg_allowports_eks" {
     type = list(number)
 }
 
